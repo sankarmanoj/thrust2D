f3cb0d9abf801bd814ca73225d74f2ee0b8956c6
Ajai V George <ajaivgeorgedev@gmail.com>
2016-09-27 13:44:13 +0530

    matrixmult, transpose

--- include/thrust/system/cuda/window_transform.h
+++ include/thrust/system/cuda/window_transform.h
@@ -18,5 +18,7 @@ namespace thrust
     void transform(cuda::shared_policy,Iterator begin1, Iterator end1, Iterator begin2, Func f);
     template <class Iterator, class Func>
     void transform(cuda::shared_policy,Iterator begin1, Iterator end1, Iterator begin2, Iterator begin3, Func f);
+    Block_2D<T> matrix_multiply(Block_2D<T> a, Block_2D<T> b);
+    void transpose(Block_2D<T> a)
 }
 #include <thrust/system/cuda/window_transform.inl>
