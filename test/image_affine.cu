7710c3c8f5485bec80ae9b4f8721c6076f3893bd
Sankar Manoj <sankar.manoj@me.com>
2016-10-18 14:22:06 +0530

    Edge Dectection .

--- test/image_affine.cu
+++ test/image_affine.cu
@@ -7,6 +7,15 @@
 #include <math.h>
 using namespace cv;
 // #define AFFINE
+
+#define PI 3.14159
+using namespace cv;
+inline float gauss(int x, int y, int mid, float sigma )
+{
+  float temp = (pow(x-mid,2)+pow(y-mid,2))/sigma;
+  temp= exp(-temp);
+  return temp;
+}
 #define HARRIS
 class AffineTransformFunctor : public thrust::shared_window_for_each_functor<float>
 {
@@ -28,6 +37,65 @@ public:
     (*outBlock)[y_out][x_out]=inputWindow[0][0];
   }
 };
+void getGaussianKernelBlock(int dim, float sigma,thrust::block_2d<float> &GaussianKernel )
+{
+  assert(dim%2);
+  float inverseSigmaSquare;
+  inverseSigmaSquare = 1/pow(sigma,2);
+  float pi2SigSquare;
+  pi2SigSquare = inverseSigmaSquare*2*PI;
+  int mid = (dim-1)/2;
+  float total = 0;
+  for(int i = 0; i<dim;i++)
+  {
+    for(int j = 0; j<dim;j++)
+    {
+      total+=gauss(i,j,mid,sigma);
+      (GaussianKernel)[i][j]=gauss(i,j,mid,sigma);
+    }
+  }
+
+  float newTotal=0;
+  for(int i = 0; i<dim;i++)
+  {
+    for(int j = 0; j<dim;j++)
+    {
+      (GaussianKernel)[i][j]/=total;
+      newTotal +=  (GaussianKernel)[i][j];
+    }
+  }
+  printf("Total = %f,newTotal=%f\n",total,newTotal);
+}
+class HarrisIntensityFunctor : public thrust::shared_unary_window_transform_functor<float>
+{
+
+public:
+  thrust::block_2d<float> * kernel;
+  HarrisIntensityFunctor(thrust::block_2d<float> * kernel)
+  {
+    this->kernel = kernel;
+  }
+  __device__ void operator() (const thrust::window_2d<float> &inputWindow,const thrust::window_2d<float> &outputWindow) const
+  {
+    float intensityValue;
+
+    for(int xoffset = 1 ; xoffset <=1 ; xoffset++)
+    {
+      for(int yoffset = 1 ; yoffset <=1 ; yoffset++)
+      {
+        for(int i = 0; i< 3; i++)
+        {
+          for(int j = 0; j< 3; j++)
+          {
+              intensityValue += ((*kernel)[i][j])*(inputWindow[i + yoffset][j + xoffset]-inputWindow[i][j]);
+          }
+        }
+      }
+    }
+    outputWindow[2][2]=intensityValue*0.1;
+  }
+};
+
 int main(int argc, char const *argv[]) {
   Mat small = imread("car.jpg",CV_LOAD_IMAGE_GRAYSCALE);
   Mat image;
@@ -111,7 +179,7 @@ int main(int argc, char const *argv[]) {
     imwrite("aoutput.png",output);
   #endif
 
-  #ifdef HARRIS
+  #ifdef SOBELDERIV
   thrust::block_2d<float> XDeriv (image.cols,image.rows);
   thrust::block_2d<float> YDeriv (image.cols,image.rows);
   thrust::copy(float_image_block.begin(),float_image_block.end(),XDeriv.begin());
@@ -151,7 +219,28 @@ int main(int argc, char const *argv[]) {
   imwrite("yout.png",yout);
   #endif
 
+  #ifdef HARRIS
+  thrust::block_2d<float> kernel(3,3);
+  getGaussianKernelBlock(3,5,kernel);
+  thrust::window_vector<float> inputVector = thrust::window_vector<float>(&float_image_block,5,5,1,1);
+  thrust::window_vector<float> outputVector = thrust::window_vector<float>(&outBlock,5,5,1,1);
+  thrust::transform(thrust::cuda::shared,inputVector.begin(),inputVector.end(),outputVector.begin(),HarrisIntensityFunctor(kernel.device_pointer));
 
+  unsigned char * outputFloatImageData = (unsigned char *)malloc(sizeof(unsigned char)*(float_image_block.end()-float_image_block.begin()));
+  cudaMemcpy(img,thrust::raw_pointer_cast(outBlock.data()),sizeof(float)*(float_image_block.end()-float_image_block.begin()),cudaMemcpyDeviceToHost);
+  for(int i = 0; i<image.cols*image.rows;i++)
+  {
+    outputFloatImageData[i]=(unsigned char)img[i];
+  }
+  Mat output (Size(image.cols,image.rows),CV_8UC1,outputFloatImageData);
+  // std::cout<<output;
+  // cudaCheckError();
+  // std::cout<<output.type()<<"  "<<Size(image.cols,image.rows)<<"="<<image_block.end()-image_block.begin()<<"\n";
+  imshow("ainput.png",image);
+  // imshow("output",output);
+  imshow("aoutput.png",output);
+  waitKey(0);
+  #endif
   // waitKey(0);
   // std::cout<<float_image<<"\n";
   return 0;
