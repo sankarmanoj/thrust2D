34604c31cf4970c532d15dc3da5377fe3615bf81
Sankar Manoj <sankar.manoj@me.com>
2017-03-01 15:21:12 +0530

    PF profiling

--- include/thrust/system/cuda/constant_memory.h
+++ include/thrust/system/cuda/constant_memory.h
@@ -2,7 +2,7 @@
 #include <thrust/device_vector.h>
 namespace thrust
 {
-  __constant__ unsigned char c_memory[65536];
+  __constant__ unsigned char c_memory[55536];
   static int c_position = 0;
   template<class T>
   T* get_constant_memory_pointer(thrust::detail::normal_iterator<thrust::device_ptr<T>>, thrust::detail::normal_iterator<thrust::device_ptr<T>>);
