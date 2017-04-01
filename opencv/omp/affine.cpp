9342fca9a35e7210f406f8d21040fd6bb125f69c
Sankar Manoj <sankar.manoj@me.com>
2017-04-01 14:42:40 +0530

    OpenMP

---
+++ opencv/omp/affine.cpp
@@ -0,0 +1,75 @@
+#define THRUST_DEVICE_SYSTEM 2
+#include <opencv2/opencv.hpp>
+#include <thrust/window_2d.h>
+using namespace cv;
+class AffineTransformFunctor
+{
+public:
+  thrust::block_2d<float> *transformMatrix;
+  thrust::block_2d<uchar> *outBlock;
+
+  AffineTransformFunctor(thrust::block_2d<float> * tm,thrust::block_2d<uchar> * outBlock)
+  {
+    this->transformMatrix = tm->device_pointer;
+    this->outBlock = outBlock->device_pointer;
+  }
+  __device__ __host__ void operator() (const thrust::window_2d<uchar> &inputWindow) const
+  {
+    int x_out, y_out;
+    x_out = (int)((*transformMatrix)[0][0]*inputWindow.start_x+(*transformMatrix)[0][1]*inputWindow.start_y+(*transformMatrix)[0][2]*1);
+    y_out = (int)((*transformMatrix)[1][0]*inputWindow.start_x+(*transformMatrix)[1][1]*inputWindow.start_y+(*transformMatrix)[1][2]*1);
+
+    (*outBlock)[y_out][x_out]=inputWindow[0][0];
+  }
+};
+int main(int argc, char const *argv[]) {
+  Mat small = imread("car.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+  Mat image;
+  image = small;
+  thrust::block_2d<uchar> uchar_image_block (image.cols,image.rows,0.0f);
+  thrust::block_2d<uchar> outBlock (image.cols,image.rows,0.0f);
+  uchar * img = (uchar * )malloc(sizeof(uchar)*(image.cols*image.rows));
+  for(int i = 0; i<image.cols*image.rows;i++)
+  {
+    img[i]=(uchar)image.ptr()[i];
+  }
+  uchar_image_block.upload(img);
+  Point2f srcTri[3];
+  Point2f dstTri[3];
+  Mat warp_mat( 2, 3, CV_32FC1 );
+  /// Set your 3 points to calculate the  Affine Transform
+  srcTri[0] = Point2f( 0,0 );
+  srcTri[1] = Point2f( image.cols - 1, 0 );
+  srcTri[2] = Point2f( 0, image.rows - 1 );
+  dstTri[0] = Point2f( image.cols*0.0, image.rows*0.5 );
+  dstTri[1] = Point2f( image.cols*0.8, image.rows*0.2 );
+  dstTri[2] = Point2f( image.cols*0.2, image.rows*0.7 );
+  /// Get the Affine Transform
+  warp_mat = getAffineTransform( srcTri, dstTri );
+  warp_mat.convertTo(warp_mat,CV_32FC1);
+  thrust::host_block_2d<float> host_warp_block(warp_mat.cols,warp_mat.rows);
+  thrust::block_2d<float> warp_block(warp_mat.cols,warp_mat.rows);
+  for(int i = 0; i< warp_mat.rows;i++)
+  {
+    for(int j = 0; j<warp_mat.cols;j++)
+    {
+      host_warp_block[i][j]=warp_mat.at<float>(i,j);
+    }
+  }
+  warp_block = host_warp_block;
+  //Create Windows For Indexing
+  thrust::window_vector<uchar> inputVector(&uchar_image_block,1,1,1,1);
+  AffineTransformFunctor atf(&warp_block,&outBlock);
+  thrust::for_each(inputVector.begin(),inputVector.end(),atf);
+  cudaDeviceSynchronize();
+  unsigned char * outputFloatImageData = (unsigned char *)malloc(sizeof(unsigned char)*(uchar_image_block.end()-uchar_image_block.begin()));
+  outBlock.download(&img);
+  for(int i = 0; i<image.cols*image.rows;i++)
+  {
+    outputFloatImageData[i]=(unsigned char)img[i];
+  }
+  Mat output (Size(image.cols,image.rows),CV_8UC1,outputFloatImageData);
+  imwrite("input.png",image);
+  imwrite("aoutput.png",output);
+  return 0;
+}
---
+++ opencv/omp/dilate.cpp
@@ -0,0 +1,55 @@
+#define THRUST_DEVICE_SYSTEM 2
+#include <opencv2/opencv.hpp>
+#include <thrust/window_2d.h>
+using namespace cv;
+class dilateFunctor //: public thrust::shared_unary_window_transform_functor<uchar>
+{
+  public:
+  __device__ uchar operator() (const thrust::window_2d<uchar> &inputWindow,const thrust::window_2d<uchar> &outputWindow) const
+  {
+    uchar temp = 0;
+    for(int i = 0; i<inputWindow.window_dim_y;i++)
+    {
+      for(int j = 0; j<inputWindow.window_dim_x;j++)
+      {
+        temp = max((float)temp,(float)inputWindow[i][j]);
+      }
+    }
+    outputWindow[inputWindow.window_dim_y/2][inputWindow.window_dim_x/2]=temp;
+    return 0;
+  }
+};
+int main(int argc, char const *argv[]) {
+  Mat small = imread("car.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+  Mat image;
+  resize(small,image,Size(512,512));
+  thrust::block_2d<unsigned char > image_block (image.cols,image.rows);
+  thrust::block_2d<uchar> uchar_image_block (image.cols,image.rows);
+  thrust::block_2d<uchar> outBlock (image.cols,image.rows);
+  thrust::block_2d<uchar> null_block (image.cols,image.rows);
+  uchar * img = (uchar * )malloc(sizeof(uchar)*(image_block.end()-image_block.begin()));
+  for(int i = 0; i<image.cols*image.rows;i++)
+  {
+    img[i]=(uchar)image.ptr()[i];
+  }
+  uchar_image_block.upload(img);
+  cudaMemcpy2D(uchar_image_block.data_pointer,uchar_image_block.pitch,img,image.cols*sizeof(uchar),image.cols,image.rows,cudaMemcpyHostToDevice);
+  thrust::window_vector<uchar> myVector = thrust::window_vector<uchar>(&uchar_image_block,3,3,1,1);
+  thrust::window_vector<uchar> outputVector = thrust::window_vector<uchar>(&outBlock,3,3,1,1);
+  #pragma omp parallel
+{
+    printf("Thread Number = %d\n", omp_get_thread_num());
+}
+  thrust::transform(myVector.begin(),myVector.end(),outputVector.begin(),null_block.begin(),dilateFunctor());
+  unsigned char * outputFloatImageData = (unsigned char *)malloc(sizeof(unsigned char)*(uchar_image_block.end()-uchar_image_block.begin()));
+  outBlock.download(&img);
+  for(int i = 0; i<image.cols*image.rows;i++)
+  {
+    outputFloatImageData[i]=(unsigned char)img[i];
+  }
+  Mat output (Size(image.cols,image.rows),CV_8UC1,outputFloatImageData);
+  imwrite("input.png",image);
+  imwrite("output.png",output);
+
+  return 0;
+}
