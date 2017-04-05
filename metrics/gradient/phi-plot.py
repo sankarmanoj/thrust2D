f4187af07e5f0f565eeb6dbdca5d2ddd43e04e7a
Sankar Manoj <sankar.manoj@me.com>
2017-04-05 21:40:05 +0530

    Plotting for PF Omp

---
+++ metrics/gradient/phi-plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import sys
+import json
+from os.path import dirname, abspath
+import os
+original_path  = os.getcwd()
+path = os.getcwd().replace("metrics/","")
+print path
+files = ["native_gr.log","thrust_gr.log"]
+data  = []
+for f in files:
+    data.append(open(path+"/"+f,"r"))
+sizes = range(100,20000,100)
+print len(sizes)
+names = {data[0]:"Native",data[1]:"Thrust"}
+plt.xlabel("Dimension")
+plt.ylabel("Time")
+print sys.argv
+plt.title(sys.argv[1])
+for d  in data:
+    dims = []
+    total = []
+    for x in sizes:
+        dims.append(x)
+        val = d.readline()
+        try:
+            total.append(float(val))
+        except:
+            print val
+    plt.plot(dims,total,label=names[d])
+
+plt.legend(loc=2)
+
+plt.show()
