443e32548e90b39de499118d36c08ec6fb743621
sankarmanoj <me@sankar-manoj.com>
2017-05-24 14:52:04 +0530

    WOrking Profiling for hotspot

--- metrics/rodinia/hotspot/test.py
+++ metrics/rodinia/hotspot/test.py
@@ -10,11 +10,11 @@ execs = [ x for x in  os.listdir(path) if x.partition(".")[2]=="out" ]
 print execs
 for texec in execs:
     results[texec]=[]
-dims = [2**x for x in range(6,14)]
+dims = [2**x for x in range(6,10)]
+dims.remove(512)
 for dim in dims:
     print str(dim)+"  ",
     for texec in execs:
-        times = {}
         print texec,
         print os.popen("nvprof -u us --csv --log-file log.txt ./%s %d 1 4 temp_512 power_512 result"%(texec,dim)).read()
         with open("log.txt","r") as x:
@@ -27,15 +27,13 @@ for dim in dims:
             line = cr.next()
             values = len(line)
             while values >= 6 :
-                if "mempcy" not in line[6]:
+                if "memcpy" not in line[6] and "fill" not in line[6]:
                     try:
-                        times[line[6]]
+                        results[texec].append((float(line[3]),dim))
                     except:
-                        times[line[6]]=[]
-                    times[line[6]].append((float(line[3]),dim))
+                        pass
                 line = cr.next()
                 values = len(line)
-    results[texec].append(times)
     print "\n"
 
 os.system("rm log.txt")
