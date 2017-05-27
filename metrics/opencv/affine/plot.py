d4412ddad8b6e1e352f1da8dd4e166c57e094a73
sankarmanoj <me@sankar-manoj.com>
2017-05-27 13:49:33 +0530

    Test and Plot for affine

---
+++ metrics/opencv/affine/plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import sys
+import json
+data = json.load(open("result.json","r"))
+
+
+plt.xlabel("Dimension")
+plt.ylabel("Time in Microseconds")
+names = {"non_shared/affine.o":"Non Shared","shared/affine.o":"Shared","opencv/affine.o":"Native"}
+colors = {"Non Shared":"r","Shared":"g","Native":"b"}
+for app in data:
+    dims = []
+    total = []
+    for val in app["values"]:
+        dims.append(val[0])
+        total.append(val[1])
+    plt.plot(dims,total,colors[names[app[".name"]]],label=names[app[".name"]])
+
+plt.legend(loc=2)
+
+plt.show()
---
+++ metrics/rodinia/hotspot/plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import sys
+import json
+data = json.load(open("result.json","r"))
+
+
+plt.xlabel("Dimension")
+plt.ylabel("Time in Microseconds")
+plt.title(sys.argv[1])
+keys = data.keys()
+print keys
+names={"hotspot_foreach.out":"Thrust","hotspot_cuda.out":"Native"}
+keys = sorted(keys,key=lambda k:names[k])
+colors = {"Thrust":"r","Native":"b"}
+for key in keys:
+    dims = []
+    dot = []
+    grad = []
+    total = []
+    for val in data[key]:
+        dims.append(val[1])
+        total.append(val[0])
+    plt.plot(dims,total,colors[names[key]],label=names[key])
+    # plt.plot(dims,total,label=key[:-2]+" Total")
+    # plt.plot(dims,dot,label=key+" Dot")
+    # plt.plot(dims,grad,label=key+" Grad")
+plt.legend(loc=2)
+plt.show()
