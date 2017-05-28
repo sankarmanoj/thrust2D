173b2d67886e77624af58db5a28935a7d1f9e778
sankarmanoj <me@sankar-manoj.com>
2017-05-28 21:13:08 +0530

    Added profling for Convolution Seperable

---
+++ metrics/nvidia_samples/convolutionSeparable/test.py
@@ -0,0 +1,43 @@
+import os
+import csv
+import json
+original_path  = os.getcwd()
+path = os.getcwd().replace("metrics/","")
+os.chdir(path)
+print path
+results = []
+execs = execs = [ x for x in  os.listdir(path) if ".out" in x ]
+print execs
+dims = [x*128 for x in range(1,20)]
+for texec in execs:
+    times = {".name":texec}
+    print texec,
+    for dim in dims:
+        print os.popen("nvprof -u us --csv --log-file log.txt ./%s %d"%(texec,dim)).read()
+        print dim
+        with open("log.txt","r") as x:
+            cr = csv.reader(x)
+            cr.next()
+            cr.next()
+            cr.next()
+            cr.next()
+            cr.next()
+            line = cr.next()
+            values = len(line)
+            while values >= 6 :
+                if "memcpy" not in line[6] and "memset" not in line[6] and "fill" not in line[6]:
+                    try:
+                        times[line[6]]
+                    except:
+                        times[line[6]]=[]
+                    times[line[6]].append((dim,float(line[3])))
+                line = cr.next()
+                values = len(line)
+    results.append(times)
+
+
+print "\n"
+
+os.system("rm log.txt")
+os.chdir(original_path)
+json.dump(results,open("result.json","w"),indent=4,sort_keys = True, separators=(',', ': '))
